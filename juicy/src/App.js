5f45e5d520bacbe752b8b2c3006ecce25f20c60b
Erik Rodriguez <perhapserik@gmail.com>
2021-04-13 15:11:17 -0700

    • Adds html for home section

--- juicy/src/App.js
+++ juicy/src/App.js
@@ -2,8 +2,31 @@ import "./index.css";
 
 function App() {
   return (
-    <div>
-      <p>app.js file here</p>
+    <div className="app">
+      <section className="home">
+        <header>
+          <h1>JUICY</h1>
+        </header>
+
+        <div className="hero">
+          <h2>
+            Fresh fruit products.
+            <br />
+            <br />
+            Shipped to you.
+            <br />
+            <br />
+            Every week.
+          </h2>
+
+          <div className="heroImages">
+            {/* <img src={} alt="" />
+            <img src={} alt="" /> */}
+          </div>
+        </div>
+
+        <div className="botCont">{/* <img src={} alt="" /> */}</div>
+      </section>
     </div>
   );
 }
