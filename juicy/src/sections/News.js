db7daa6edbb5aaf13a13348842772455f942e011
Erik Rodriguez <perhapserik@gmail.com>
2021-05-15 11:41:07 -0700

    • Adds reveal animation to news articles in news.js section folder

--- juicy/src/sections/News.js
+++ juicy/src/sections/News.js
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 // components
 import NewsCards from "../components/NewsCards.js";
 // data structure
@@ -5,10 +6,34 @@ import { ndata } from "../utils/structures/NewsStructure.js";
 // assets
 import news1 from "../assets/videos/juicy_news_1_by_KoolShooters_from_Pexels.mp4";
 import news2 from "../assets/videos/juicy_news_2_by_KoolShooters_from_Pexels.mp4";
+// scroll animation
+import gsap, { Bounce } from "gsap";
 // stylesheet
 import "../index.css";
 
 export default function News() {
+  // const videoRef = useRef(null);
+
+  const newsContTl = gsap.timeline();
+
+  useEffect(() => {
+    // news video slide animation
+    newsContTl.staggerFrom(
+      ".newsCard",
+      3,
+      {
+        autoAlpha: 0,
+        scrollTrigger: {
+          trigger: ".news",
+          start: "top+=25% center",
+          end: "+=5% top",
+          markers: true,
+          scrub: 2,
+        },
+      },
+      0.5
+    );
+  }, [newsContTl]);
   return (
     <section className="news">
       <div className="title">
@@ -22,15 +47,17 @@ export default function News() {
         </video>
       </div>
 
-      {ndata.map((newsObj) => (
-        <NewsCards key={newsObj.id} data={newsObj} />
-      ))}
-      <div className="videoCenter">
+      <div className="newsGrid">
+        {ndata.map((newsObj) => (
+          <NewsCards key={newsObj.id} data={newsObj} />
+        ))}
+      </div>
+      {/* <div className="videoCenter">
         <video loop autoPlay muted width="500px" height="600px">
           <source src={news2} type="video/mp4" />
           Browser does not support this video.
         </video>
-      </div>
+      </div> */}
     </section>
   );
 }
