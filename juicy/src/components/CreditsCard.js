ff9a146fe7197d178a5fd8d5d77f3d675ca0b8e1
Erik Rodriguez <perhapserik@gmail.com>
2021-05-23 16:08:34 -0700

    • Adds media queries support for news and credits section • Fixes credits anchors size to a suitable width

--- juicy/src/components/CreditsCard.js
+++ juicy/src/components/CreditsCard.js
@@ -3,13 +3,10 @@ import "../index.css";
 export default function CreditsCard(props) {
   const { name, profileLink } = props.data;
   return (
-    <a
-      href={profileLink}
-      className="creditsCard"
-      target="_blank"
-      rel="noreferrer"
-    >
-      <p>{name}</p>
-    </a>
+    <div className="creditsCard">
+      <a href={profileLink} target="_blank" rel="noreferrer">
+        <p>{name}</p>
+      </a>
+    </div>
   );
 }
